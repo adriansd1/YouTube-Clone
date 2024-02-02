d000d3d8c86713a9d3967fee3d8b4069f1f06074
adriansd1 <154755212+adriansd1@users.noreply.github.com>
2024-02-01 20:41:03 -0500

    Update ytapp.js
    Privacy Leak Fix

--- ytapp.js
+++ ytapp.js
@@ -1,6 +1,6 @@
 const videoCardContainer = document.querySelector('.video-container');
 
-let api_key = "AIzaSyBg4ygU2g2FLNFfzaQa0sTQgbt9Bg6sI5I";
+let api_key = "";
 let video_http = "https://www.googleapis.com/youtube/v3/videos?";
 let channel_http = "https://www.googleapis.com/youtube/v3/channels?"
 
